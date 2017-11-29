a5b0c6dc1eed4ddee42a4ecbcdef73180ded9eba
Paul Colomiets <paul@colomiets.name>
2017-11-29 20:33:12 +0200

    Upgrades tk-pool

--- src/connect.rs
+++ src/connect.rs
@@ -2,35 +2,34 @@ use std::net::SocketAddr;
 
 use futures::{Stream, Future};
 use futures::future::{FutureResult, ok, empty};
-use tk_http::client::{Proto, Config as HConfig, Codec};
+use tk_http::client::{Proto, Config, Codec};
 use tk_http::client::{Error, EncoderDone};
-use tk_pool::uniform::{UniformMx, Config as PConfig};
-use tk_pool;
+use tk_pool::{self, pool_for};
 use tokio_core::reactor::Handle;
 use tokio_core::net::TcpStream;
 
 use {Connection};
 
-pub type Pool = tk_pool::Pool<Box<Codec<TcpStream,
-        Future=FutureResult<EncoderDone<TcpStream>, Error>>>>;
+pub type Pool = tk_pool::queue::Pool<Box<Codec<TcpStream,
+        Future=FutureResult<EncoderDone<TcpStream>, Error>>>,
+        tk_pool::metrics::Noop>;
 
 /// Connects to the cantal instance on localhost
 pub fn connect_local(h: &Handle) -> Connection {
     let h1 = h.clone();
-    let pool_config = PConfig::new()
-        .connections_per_address(1)
-        .done();
-    let connection_config = HConfig::new()
+    let connection_config = Config::new()
         .inflight_request_limit(2)
         .done();
-    let multiplexer = UniformMx::new(&h,
-        &pool_config,
-        ok("127.0.0.1:22682".parse::<SocketAddr>().unwrap().into())
-            .into_stream()
-            .chain(empty::<_, Error>().into_stream()),
-        move |addr| Proto::connect_tcp(addr, &connection_config, &h1));
-    let queue_size = 10;
-    let pool = tk_pool::Pool::create(&h, queue_size, multiplexer);
+    let pool = pool_for(move |addr| {
+            Proto::connect_tcp(addr, &connection_config, &h1)
+        })
+        .connect_to(
+            ok("127.0.0.1:22682".parse::<SocketAddr>().unwrap().into())
+                .into_stream()
+                .chain(empty().into_stream()))
+        .lazy_uniform_connections(1)
+        .with_queue_size(1)
+        .spawn_on(h);
     return Connection {
         pool: pool,
     }
