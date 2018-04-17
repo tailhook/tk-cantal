455fa5866d97f560d8284f057feec7d1a00a8df1
Paul Colomiets <paul@colomiets.name>
2018-04-17 19:45:56 +0300

    Upgrade env_logger in env dependencies

--- examples/monitor_peers.rs
+++ examples/monitor_peers.rs
@@ -13,7 +13,7 @@ fn main() {
     if let Err(_) = env::var("RUST_LOG") {
         env::set_var("RUST_LOG", "warn");
     }
-    env_logger::init().unwrap();
+    env_logger::init();
     tk_easyloop::run(|| {
         let conn = connect_local(&tk_easyloop::handle());
         tk_easyloop::interval(Duration::new(10, 0))
